5bb57169b1f4a27b24dfe863ebb0656f54c64528
PurityKM <irungu1210@gmail.com>
2023-01-24 14:06:07 +0300

    Commits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+
+import "./App.css";
 
 function App() {
+  const title = "My first website";
+  const name = "Purity Irungu";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>{ name } website</p>
+      </div>
     </div>
   );
 }
